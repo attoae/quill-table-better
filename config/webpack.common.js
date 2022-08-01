e110ccbae24f1c45ad80273529ec8f9f1d98a3d0
974616960 <974616960@qq.com>
2022-08-01 22:49:09 +0800

    create project

---
+++ config/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  resolve: {
+    extensions: ['.ts', '.scss', '.js']
+  }
+};
