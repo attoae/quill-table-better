b0e58f3de099d20e697f501f0a4d0be406e76311
attoae <55045456+attoae@users.noreply.github.com>
2023-02-09 21:18:42 +0800

    cell-selection

--- src/ui/cell-selection.ts
+++ src/ui/cell-selection.ts
@@ -1,31 +1,52 @@
-import { setElementProperty } from '../utils';
+import Quill from 'quill';
+import { setElementProperty, getCorrectBounds } from '../utils';
+import { TableCell } from '../formats/table';
 
-interface Options {
-  tableNode: Element
-  cellNode: Element
+interface CorrectBound {
+  left: number;
+  top: number;
+  right: number;
+  bottom: number;
+  width?: number;
+  height?: number;
 }
+
+const DEVIATION = 2;
 
 class CellSelection {
   quill: any;
-  options: Options;
-  selected: Element[];
-  constructor(quill: any, options: Options) {
+  table: Element;
+  selectedTds: Element[];
+  constructor(quill: any, table: Element) {
+    // if (table) return;
     this.quill = quill;
-    this.options = options;
-    this.selected = [];
+    this.table = table;
+    this.selectedTds = [];
     this.quill.root.addEventListener('mousedown', this.handleMousedown.bind(this), false);
   }
 
   handleMousedown(e: MouseEvent) {
-    this.clearSelected();
     if (!(e.target as Element).closest('table')) return;
+    this.clearSelected();
     const startTd = (e.target as Element).closest('td');
-    this.selected = [startTd];
+    this.selectedTds = [startTd];
     startTd.classList.add('ql-cell-focused');
     
     const handleMouseMove = (e: MouseEvent) => {
+      this.clearSelected();
       const endTd = (e.target as Element).closest('td');
-      
+      const startCorrectBounds = getCorrectBounds(startTd, this.quill.container);
+      const endCorrectBounds = getCorrectBounds(endTd, this.quill.container);
+      const computeBounds = this.getComputeBounds(startCorrectBounds, endCorrectBounds);
+      this.selectedTds = this.getComputeSelectedTds(computeBounds, this.table);
+      if (this.selectedTds.length === 1) {
+        const td = this.selectedTds[0];
+        td.classList && td.classList.add('ql-cell-focused');
+      } else {
+        for (const td of this.selectedTds) {
+          td.classList && td.classList.add('ql-cell-selected');
+        }
+      }
       // e.preventDefault();
       if (startTd !== endTd) this.quill.blur();
     }
@@ -41,10 +62,39 @@ class CellSelection {
   }
 
   clearSelected() {
-    for (const td of this.selected) {
-      td.classList.remove('ql-cell-focused', 'ql-cell-selected');
+    for (const td of this.selectedTds) {
+      td.classList && td.classList.remove('ql-cell-focused', 'ql-cell-selected');
     }
-    this.selected = [];
+    this.selectedTds = [];
+  }
+
+  getComputeBounds(startCorrectBounds: CorrectBound, endCorrectBounds: CorrectBound) {
+    const left = Math.min(startCorrectBounds.left, endCorrectBounds.left);
+    const right = Math.max(startCorrectBounds.right, endCorrectBounds.right);
+    const top = Math.min(startCorrectBounds.top, endCorrectBounds.top);
+    const bottom = Math.max(startCorrectBounds.bottom, endCorrectBounds.bottom);
+    return { left, right, top, bottom }
+  }
+
+  getComputeSelectedTds(computeBounds: CorrectBound, table: Element): Element[] {
+    const tableParchment = Quill.find(table);
+    const tableCells = tableParchment.descendants(TableCell); 
+    return tableCells.reduce((selectedTds: Element[], tableCell: any) => {
+      const { left, top, width, height } = getCorrectBounds(tableCell.domNode, this.quill.container);
+      if (
+        left + DEVIATION >= computeBounds.left &&
+        left - DEVIATION + width <= computeBounds.right &&
+        top + DEVIATION >= computeBounds.top &&
+        top - DEVIATION + height <= computeBounds.bottom
+      ) {
+        selectedTds.push(tableCell.domNode);
+      }
+      return selectedTds;
+    }, []);
+  }
+
+  updateTable(table: Element) {
+    this.table = table;
   }
 }
 
