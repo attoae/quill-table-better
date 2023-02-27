aa33bfb5567556b6e8a1867a589c9f4c163237fa
attoae <55045456+attoae@users.noreply.github.com>
2023-02-27 22:55:15 +0800

    Implement table row and column operations

--- src/ui/cell-selection.ts
+++ src/ui/cell-selection.ts
@@ -1,25 +1,19 @@
-import Quill from 'quill';
-import { setElementProperty, getCorrectBounds } from '../utils';
-import { TableCell } from '../formats/table';
+import {
+  setElementProperty,
+  getCorrectBounds,
+  getComputeBounds,
+  getComputeSelectedTds
+} from '../utils';
 import { Blot } from 'parchment/dist/typings/blot/abstract/blot';
-
-interface CorrectBound {
-  left: number;
-  top: number;
-  right: number;
-  bottom: number;
-  width?: number;
-  height?: number;
-}
-
-const DEVIATION = 2;
 
 class CellSelection {
   quill: any;
   selectedTds: Element[];
+  endTd: Element;
   constructor(quill: any) {
     this.quill = quill;
     this.selectedTds = [];
+    this.endTd = null;
     this.quill.root.addEventListener('mousedown', this.handleMousedown.bind(this));
   }
 
@@ -28,6 +22,7 @@ class CellSelection {
     const table = (e.target as Element).closest('table');
     if (!table) return;
     const startTd = (e.target as Element).closest('td');
+    this.endTd = startTd;
     this.selectedTds = [startTd];
     startTd.classList.add('ql-cell-focused');
     
@@ -37,10 +32,11 @@ class CellSelection {
       const isEqualNode = startTd.isEqualNode(endTd);
       if (isEqualNode) return;
       this.clearSelected();
+      this.endTd = endTd;
       const startCorrectBounds = getCorrectBounds(startTd, this.quill.container);
       const endCorrectBounds = getCorrectBounds(endTd, this.quill.container);
-      const computeBounds = this.getComputeBounds(startCorrectBounds, endCorrectBounds);
-      this.selectedTds = this.getComputeSelectedTds(computeBounds, table);
+      const computeBounds = getComputeBounds(startCorrectBounds, endCorrectBounds);
+      this.selectedTds = getComputeSelectedTds(computeBounds, table, this.quill.container);
       for (const td of this.selectedTds) {
         td.classList && td.classList.add('ql-cell-selected');
       }
@@ -61,31 +57,6 @@ class CellSelection {
       td.classList && td.classList.remove('ql-cell-focused', 'ql-cell-selected');
     }
     this.selectedTds = [];
-  }
-
-  getComputeBounds(startCorrectBounds: CorrectBound, endCorrectBounds: CorrectBound) {
-    const left = Math.min(startCorrectBounds.left, endCorrectBounds.left);
-    const right = Math.max(startCorrectBounds.right, endCorrectBounds.right);
-    const top = Math.min(startCorrectBounds.top, endCorrectBounds.top);
-    const bottom = Math.max(startCorrectBounds.bottom, endCorrectBounds.bottom);
-    return { left, right, top, bottom }
-  }
-
-  getComputeSelectedTds(computeBounds: CorrectBound, table: Element): Element[] {
-    const tableParchment = Quill.find(table);
-    const tableCells = tableParchment.descendants(TableCell); 
-    return tableCells.reduce((selectedTds: Element[], tableCell: any) => {
-      const { left, top, width, height } = getCorrectBounds(tableCell.domNode, this.quill.container);
-      if (
-        left + DEVIATION >= computeBounds.left &&
-        left - DEVIATION + width <= computeBounds.right &&
-        top + DEVIATION >= computeBounds.top &&
-        top - DEVIATION + height <= computeBounds.bottom
-      ) {
-        selectedTds.push(tableCell.domNode);
-      }
-      return selectedTds;
-    }, []);
   }
 }
 
