e2020ff84b4439df6ec0cd538ec24be457289d2f
attoae <974616960@qq.com>
2024-09-05 16:19:24 +0800

    fix: History with list and header

--- src/formats/list.ts
+++ src/formats/list.ts
@@ -1,14 +1,24 @@
 import Quill from 'quill';
 import { TableCell, TableCellBlock } from './table';
 import { getCellFormats, getCorrectCellBlot } from '../utils';
+import { CELL_ATTRIBUTE } from '../config';
 
 const List = Quill.import('formats/list');
 const Container = Quill.import('blots/container');
 
 class ListContainer extends Container {
-  static create(value: string) {
+  static create(value: Props) {
     const node = super.create();
-    node.setAttribute('data-cell', value);
+    const keys = Object.keys(value);
+    for (const key of keys) {
+      if (key === 'data-row') {
+        node.setAttribute(key, value[key]);
+      } else if (key === 'cellId') {
+        node.setAttribute('data-cell', value[key]);
+      } else {
+        node.setAttribute(`data-${key}`, value[key]);
+      }
+    }
     return node;
   }
   
@@ -17,7 +27,15 @@ class ListContainer extends Container {
   }
 
   static formats(domNode: HTMLElement) {
-    return domNode.getAttribute('data-cell');
+    const formats = CELL_ATTRIBUTE.reduce((formats: Props, attr) => {
+      const name = attr.includes('data') ? attr : `data-${attr}`;
+      if (domNode.hasAttribute(name)) {
+        formats[attr] = domNode.getAttribute(name);
+      }
+      return formats;
+    }, {});
+    formats['cellId'] = domNode.getAttribute('data-cell');
+    return formats;
   }
 
   formats() {
@@ -30,7 +48,7 @@ ListContainer.className = 'table-list-container';
 ListContainer.tagName = 'OL';
 
 class TableList extends List {
-  format(name: string, value: string, isReplace?:boolean) {
+  format(name: string, value: string | Props, isReplace?: boolean) {
     const list = this.formats()[this.statics.blotName];
     if (name === 'list') {
       const [formats, cellId] = this.getCellFormats(this.parent);
@@ -41,24 +59,23 @@ class TableList extends List {
         return this.replaceWith(this.statics.blotName, value);
       }
     } else if (name === ListContainer.blotName) {
-      const cellBlot = getCorrectCellBlot(this.parent);
-      if (cellBlot) {
-        const [formats] = getCellFormats(cellBlot);
-        this.wrap(TableCell.blotName, formats);
-      } else {
-        this.wrap(TableCell.blotName, {});
+      if (typeof value === 'string') {
+        value = { cellId: value };
       }
-      this.wrap(name, value);
+      const [formats, cellId] = this.getCorrectCellFormats(value);
+      this.wrap(TableCell.blotName, formats);
+      this.wrap(name, { ...formats, cellId });
     } else if (name === 'header') {
       const [formats, cellId] = this.getCellFormats(this.parent);
       this.setReplace(isReplace, formats);
       return this.replaceWith('table-header', { cellId, value });
     } else if (name === TableCell.blotName) {
+      const listContainer = this.getListContainer(this.parent);
+      if (!listContainer) return;
+      const formats = listContainer.formats()[listContainer.statics.blotName];
       this.wrap(name, value);
-      if (this.parent.statics.blotName === ListContainer.blotName) {
-        const cellId = this.parent.formats()[this.parent.statics.blotName];
-        cellId && this.wrap(ListContainer.blotName, cellId);
-      }
+      // @ts-ignore
+      this.wrap(ListContainer.blotName, { ...formats, ...value });
     } else {
       super.format(name, value);
     }
@@ -67,6 +84,31 @@ class TableList extends List {
   getCellFormats(parent: TableCell) {
     const cellBlot = getCorrectCellBlot(parent);
     return getCellFormats(cellBlot);
+  }
+
+  getCorrectCellFormats(value: Props): [Props, string] {
+    const cellBlot = getCorrectCellBlot(this.parent);
+    if (!cellBlot) {
+      const cellId = value['cellId'];
+      const formats = { ...value };
+      delete formats['cellId'];
+      return [formats, cellId];
+    } else {
+      const [formats, cellId] = getCellFormats(cellBlot);
+      const _formats = { ...formats, ...value };
+      delete _formats['cellId'];
+      return [_formats, cellId];
+    }
+  }
+
+  private getListContainer(blot: ListContainer) {
+    while (blot) {
+      if (blot.statics.blotName === ListContainer.blotName) {
+        return blot;
+      }
+      blot = blot.parent;
+    }
+    return null;
   }
 
   static register() {
