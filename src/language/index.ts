6c3a1abd01c1ca9099e7310aadfbe1e42cb12826
attoae <55045456+attoae@users.noreply.github.com>
2025-04-28 23:31:43 +0800

    feat: Add portuguese language

--- src/language/index.ts
+++ src/language/index.ts
@@ -6,6 +6,7 @@ import pl_PL from './pl_PL';
 import de_DE from './de_DE';
 import ru_RU from './ru_RU';
 import tr_TR from './tr_TR';
+import pt_PT from './pt_PT';
 
 interface Config {
   [propName: string]: Props;
@@ -28,7 +29,8 @@ class Language {
       pl_PL,
       de_DE,
       ru_RU,
-      tr_TR
+      tr_TR,
+      pt_PT
     };
     this.init(language);
   }
