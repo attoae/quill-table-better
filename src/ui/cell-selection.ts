5f4d754e05f012ab83a9b4055ad7ccb73e51cf8b
knazark <nazar.kovtun@tenantcloud.com>
2025-05-19 17:25:00 +0300

    fix: Cannot read properties of null (reading 'classList') Issue: https://github.com/attoae/quill-table-better/issues/84

--- src/ui/cell-selection.ts
+++ src/ui/cell-selection.ts
@@ -318,6 +318,7 @@ class CellSelection {
     if (!table) return;
     this.tableBetter.tableMenus.destroyTablePropertiesForm();
     const startTd = (e.target as Element).closest('td');
+    if (!startTd) return;
     this.startTd = startTd;
     this.endTd = startTd;
     this.selectedTds = [startTd];
