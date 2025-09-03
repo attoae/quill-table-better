98db4fef9d1a4ac74eac13d3010b3d7ee82f3f46
Andreas Retzius <andreasretzius@gmail.com>
2025-09-03 18:07:57 +0200

    swedish language

--- src/language/index.ts
+++ src/language/index.ts
@@ -13,6 +13,7 @@ import cs_CZ from './cs_CZ';
 import da_DK from './da_DK';
 import nb_NO from './nb_NO';
 import it_IT from './it_IT';
+import sv_SE from './sv_SE';
 
 interface Config {
   [propName: string]: Props;
@@ -43,6 +44,7 @@ class Language {
       da_DK,
       nb_NO,
       it_IT,
+      sv_SE,
     };
     this.init(language);
   }
